18b4c5bd1cf30a1cdbe1933b911d88ce687913ea
Marios Bousios <mariosbousios@gmail.com>
2021-09-15 12:03:38 +0300

    Initial Commit

---
+++ Van Gogh Filter Cam Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Van Gogh Filter Cam Project
+//
+//  Created by Marios Bousios on 15/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
